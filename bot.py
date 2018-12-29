7e71cc0ad7e8473aad7245afec4be40f0519afec
Daniel <hello@danielwang.me>
2018-12-28 19:22:54 -0500

    feat: add message logging

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from loguru import logger
+
+logger.add("messages.log", enqueue=True, colorize=True,
+           format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> "
+           "<level>{message}</level>")
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    logger.info("Logged in as {0.user}", client, feature="f-strings")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    content = (message.attachments[0].proxy_url
+               if message.attachments else message.content)
+
+    logger.info("({0.guild} - #{0.channel}) {0.author}: {1}", message,
+                content, feature="f-strings")
+
+client.run(os.environ.get("DISCORD_API_KEY"))
